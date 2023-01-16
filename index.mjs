6bab78b1c1bdd72faf822669c23aad021477cd21
Naufal <80172679+naufalkharits@users.noreply.github.com>
2023-01-16 13:02:18 +0700

    refactor: revert text message

--- index.mjs
+++ index.mjs
@@ -11,8 +11,8 @@ const bot = new Telegraf(process.env.TELEGRAM_TOKEN)
 // middlewares
 app.use(express.json())
 bot.use((ctx, next) => {
-  console.log(ctx)
-  ctx.message.text = { origin: ctx.message.text, modified: ctx.message.text.toLowerCase() }
+  // ctx.message.text = { origin: ctx.message.text, modified: ctx.message.text.toLowerCase() }
+  ctx.message.text = ctx.message.text.toLowerCase()
 
   return next(ctx)
 })
@@ -20,7 +20,7 @@ bot.use((ctx, next) => {
 bot.command("start", (ctx) => ctx.reply(`Hello, ${ctx.message.from.first_name}!`))
 
 bot.command("wallet", (ctx) => {
-  const address = isAddress(ctx.message.text.modified.replace("/wallet ", ""))
+  const address = isAddress(ctx.message.text.replace("/wallet ", ""))
 
   if (!address) return ctx.reply("WRONG ADDRESS!")
 
