72339b2b1436a47e32430c576bdd748891da7d58
Naufal <80172679+naufalkharits@users.noreply.github.com>
2023-01-18 19:36:21 +0700

    refactor: refine regex

--- index.mjs
+++ index.mjs
@@ -29,14 +29,14 @@ app.post("/wallet", (req, res) => {
   console.log(req)
 })
 
-bot.hears(/\/start/i, async (ctx) => await ctx.reply(`Hello, ${ctx.message.from.first_name}!`))
+bot.hears(/^\/start\b/i, async (ctx) => await ctx.reply(`Hello, ${ctx.message.from.first_name}!`))
 
-bot.hears(/\/cancel/i, async (ctx) => {
+bot.hears(/^\/cancel\b/i, async (ctx) => {
   await ctx.conversation.exit()
   await ctx.reply("CANCELLED!")
 })
 
-bot.use(createConversation(wallet)).hears(/\/wallet/i, async (ctx) => {
+bot.use(createConversation(wallet)).hears(/^\/wallet\b/i, async (ctx) => {
   await ctx.conversation.enter("wallet")
 })
 
