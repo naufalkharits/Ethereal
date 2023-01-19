e4805a4b348af2596ee1d554d102de3bf9fd9bb3
Naufal <80172679+naufalkharits@users.noreply.github.com>
2023-01-19 18:27:03 +0700

    refactor: upgrade address validation
    to check multiple addresses

--- utils/evm.mjs
+++ utils/evm.mjs
@@ -1,23 +1,45 @@
 import keccak from "keccak"
 
-export function isAddress(address) {
-  if (!/^(0x)?[0-9a-fA-F]{40}$/.test(address)) return false
+export default function isAddress(address) {
+  const addresses = address.split(",")
 
-  return toChecksumAddress(address)
-}
-
-function toChecksumAddress(address) {
-  address = address.toLowerCase().replace("0x", "")
-  var hash = keccak("keccak256").update(address).digest("hex")
-  var ret = "0x"
-
-  for (var i = 0; i < address.length; i++) {
-    if (parseInt(hash[i], 16) >= 8) {
-      ret += address[i].toUpperCase()
-    } else {
-      ret += address[i]
-    }
+  for (let x = 0; x < addresses.length; x++) {
+    const element = addresses[x]
+    if (!/^(0x)?[0-9a-fA-F]{40}$/.test(element)) return false
   }
 
-  return ret
+  return toChecksumAddress(addresses)
+}
+
+function toChecksumAddress(addresses) {
+  const modifiedAddresses = []
+  for (let x = 0; x < addresses.length; x++) {
+    const element = addresses[x]
+    modifiedAddresses.push(element.toLowerCase().replace("0x", ""))
+  }
+
+  const hashes = []
+  for (let x = 0; x < modifiedAddresses.length; x++) {
+    const modifiedAddress = modifiedAddresses[x]
+    hashes.push(keccak("keccak256").update(modifiedAddress).digest("hex"))
+  }
+
+  let newAddress = ""
+  let newAddresses = []
+
+  for (let x = 0; x < modifiedAddresses.length; x++) {
+    const modifiedAddress = modifiedAddresses[x]
+    newAddress = "0x"
+    for (let y = 0; y < modifiedAddress.length; y++) {
+      if (parseInt(hashes[y], 16) >= 8) {
+        newAddress += modifiedAddress[y].toUpperCase()
+      } else {
+        newAddress += modifiedAddress[y]
+      }
+    }
+    newAddresses.push(newAddress)
+  }
+
+  console.log(newAddresses)
+  return newAddresses
 }
