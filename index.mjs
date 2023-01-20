754ff6ede6304bba59eab6c999bc76b3982437d3
Naufal <80172679+naufalkharits@users.noreply.github.com>
2023-01-20 15:28:09 +0700

    refactor: cleanup

--- index.mjs
+++ index.mjs
@@ -1,34 +1,19 @@
-import { conversations, createConversation } from "@grammyjs/conversations"
 import dotenv from "dotenv"
 import express from "express"
-import { Bot, session } from "grammy"
 
-import wallet from "./conversations/wallet.mjs"
+import bot from "./utils/grammy.mjs"
 
 dotenv.config()
 const app = express()
-const bot = new Bot(process.env.TELEGRAM_TOKEN)
 
 // middlewares
 app.use(express.json())
-bot.use(session({ initial: () => ({}) })).use(conversations())
-
 
 app.post("/wallet", (req, res) => {
   console.log(req)
 })
 
-bot.hears(/^\/start\b/i, async (ctx) => await ctx.reply(`Hello, ${ctx.from.first_name}!`))
-
-bot.hears(/^\/cancel\b/i, async (ctx) => {
-  await ctx.conversation.exit()
-  await ctx.reply("CANCELLED!")
-})
-
 bot.launch()
-bot
-  .use(createConversation(wallet))
-  .hears(/^\/wallet\b/i, async (ctx) => await ctx.conversation.enter("wallet"))
 
 const server = app.listen(process.env.PORT, () =>
   console.log(`listening on port ${server.address().port}`)
